db03b14e29ae419a4b5d0cbc2761310f8f0136f2
Maarten <maartenbeek15@gmail.com>
2019-04-24 09:40:56 +0200

    Map bewerking

---
+++ opdracht1/v1/script.js
@@ -0,0 +1,21 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+var meerinfo = 
+document.querySelector('#meer');
+var article = document.querySelector('body > section > article > table');
+
+function myFunction() {
+      var x = document.getElementById("meer");
+      if (x.innerHTML === "Toon minder") {
+        x.innerHTML = "Toon meer";
+      } else {
+        x.innerHTML = "Toon minder";
+      }
+    }
+
+meerinfo.addEventListener('click',function(){
+    article.classList.toggle('show-text');
+    myFunction();
+});
