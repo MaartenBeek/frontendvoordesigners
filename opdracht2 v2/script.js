5e509b2438f0b8738f76585569c7836f7745388c
Maarten <maartenbeek15@gmail.com>
2019-06-12 10:03:27 +0200

    Opdracht 3

---
+++ opdracht2 v2/script.js
@@ -0,0 +1,75 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+document.addEventListener('keydown', function showSlides() {
+    if(event.keyCode == 49) {
+        currentSlide(1);
+    }
+    if(event.keyCode == 50) {
+        currentSlide(2)
+    }
+    if(event.keyCode == 51){
+        currentSlide(3)
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- opdracht3/v1/script.js
+++ opdracht3/v1/script.js
@@ -1,6 +1,11 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
 var uri = "https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json";
 
 var section = document.querySelector('section');
+var article = document.querySelector('article');
 console.log("section",section);
 
 
@@ -10,10 +15,33 @@ request.open('get', uri);
 request.responseType = 'json';
 request.send();
 
-request.addEventListener("load", function(){
-  var data = request.response;
-  console.log("request is geladen: ",request.response);
-  //er is data
-  //nu kun je iets doen
-  section.textContent = JSON.stringify(data);
+request.addEventListener("load", function() {
+    var data = request.response;
+    populateArticle(data);
+    console.log("request is geladen: ", request.response);
+    //er is data
+    //nu kun je iets doen
+    //section.textContent = JSON.stringify(data);
 });
+
+function populateArticle(jsonObj) {
+    console.log(jsonObj.length);
+    
+    var movies = jsonObj;
+    
+    for (var i = 0; i < movies.length; i++) {
+        var myArticle = document.createElement('article');
+        myArticle.textContent = jsonObj;
+        section.appendChild(myArticle);
+        
+        var myH2 = document.createElement('h2');
+        myH2.textContent = jsonObj[0].title;
+        article.appendChild(myH2);
+
+        var myPara = document.createElement('p');
+        myPara.textContent = jsonObj[0].plot;
+        article.appendChild(myPara);
+    }
+    
+    
+}
