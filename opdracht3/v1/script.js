d967d8f9ffc25453a01c28447917e4d55141f276
Maarten <maartenbeek15@gmail.com>
2019-06-24 15:41:02 +0200

    Zoekfunctie werkend

--- opdracht3/v1/script.js
+++ opdracht3/v1/script.js
@@ -5,7 +5,6 @@
 var uri = "https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json";
 
 var section = document.querySelector('section');
-var article = document.querySelector('article');
 console.log("section",section);
 
 
@@ -31,17 +30,67 @@ function populateArticle(jsonObj) {
     
     for (var i = 0; i < movies.length; i++) {
         var myArticle = document.createElement('article');
-        myArticle.textContent = jsonObj;
         section.appendChild(myArticle);
-        
+    
         var myH2 = document.createElement('h2');
-        myH2.textContent = jsonObj[0].title;
-        article.appendChild(myH2);
+        myH2.textContent = jsonObj[i].title;
+        myArticle.appendChild(myH2);
+        
+        var myImg = document.createElement('img');
+        myImg.src = jsonObj[i].cover;
+        myArticle.appendChild(myImg);
 
         var myPara = document.createElement('p');
-        myPara.textContent = jsonObj[0].plot;
-        article.appendChild(myPara);
+        myPara.textContent = jsonObj[i].simple_plot;
+        myArticle.appendChild(myPara);
+        
+        var myDate = document.createElement('span');
+        myDate.textContent = jsonObj[i].release_date;
+        myArticle.appendChild(myDate);
+        
     }
     
-    
 }
+
+function myFunction() {
+  // Declare variables 
+  var input, filter, section, article, h2, i, txtValue, noResult;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  section = document.getElementById("mySection");
+  article = section.getElementsByTagName("article");
+  noResult = document.getElementById("noResult");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < article.length; i++) {
+    h2 = article[i].getElementsByTagName("h2")[0];
+    if (h2) {
+      txtValue = h2.textContent || h2.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        article[i].style.display = "";
+      } else {
+        article[i].style.display = "none";
+        noResult.style.display = "block";
+      }
+    } 
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
